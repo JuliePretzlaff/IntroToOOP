554ad628b1f4541aeff0d7eec9886fc85578c85d
JuliePretzlaff <juliapretzlaff@gmail.com>
2018-01-29 11:40:37 -0500

    more class work

--- IntroToOOP/Cat.cs
+++ IntroToOOP/Cat.cs
@@ -8,5 +8,52 @@ namespace IntroToOOP
 {
     class Cat
     {
+        //I need STATES
+
+        //fields
+        private string name;
+        private int age;
+        private string furColor;
+        private bool isHungry = true;
+
+        //properties
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+
+        }
+
+
+
+
+
+        //I need BEHAVIORS
+
+        //consturctors
+        public Cat()
+        {
+            //default constructor
+            //takes no parameters, nothing in parentheses)
+        }
+
+        public Cat(string name, int age, string furColor)
+        {
+            this.name = name;
+            this.age = age;
+            this.furColor = furColor;
+        }
+
+        public void Eat()
+        {
+            if (isHungry)
+            {
+                isHungry = false;
+            }
+
+            Console.WriteLine("Is the cat hungry?" + isHungry);
+        }
     }
+
+
 }
